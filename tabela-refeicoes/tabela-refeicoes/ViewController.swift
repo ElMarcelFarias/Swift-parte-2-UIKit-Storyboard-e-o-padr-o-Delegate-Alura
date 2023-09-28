95f0ab86fd4cdf8fad41d3353d130540cc3bb8d5
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-09-28 10:10:34 -0300

    first commit!

---
+++ tabela-refeicoes/tabela-refeicoes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tabela-refeicoes
+//
+//  Created by Marcel Leite de Farias on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
