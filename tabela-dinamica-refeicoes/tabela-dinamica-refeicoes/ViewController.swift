3006d6b5571400bbe223bfe4d0b780277293dcdb
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-10-03 16:36:57 -0300

    New: Finalizando parte de delegates!

--- tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
+++ tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UITableViewController {
     //para criar uma tabela dinamica precisamos de
     //numero de linhas
     
+    let refeicoes: [String] = ["churros", "macarrones", "pizza", "hotdog"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Table view controller foi carregada!")
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return refeicoes.count
     }
     
     //conteudo da celula
-    
+    //dois metodos de datasource
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
         
@@ -27,12 +34,8 @@ class ViewController: UITableViewController {
         return celula
     }
     
-    
-    let refeicoes: [String] = ["churros", "macarrones", "pizza", "hotdog"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("Table view controller foi carregada!")
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
     
     
